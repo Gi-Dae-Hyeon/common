d46dc2f7bdbdb97b83aa8b1394ec7f24dcf06642
기대현 <daehyeon@gidaehyeon-ui-MacBookPro.local>
2024-10-17 01:19:51 +0900

    feat: my dotfiles

---
+++ setting/terminal/plugins/install.mac.sh
@@ -0,0 +1,6 @@
+# /bin/zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+brew install autojump
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
